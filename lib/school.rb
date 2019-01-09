d8d3e2cf167ceebd817a9853e1520e7e95103507
Ross Jones <rossmosis.jones@gmail.com>
2019-01-09 17:21:34 -0500

    added sort method

--- lib/school.rb
+++ lib/school.rb
@@ -3,32 +3,28 @@ require 'pry'
 
 class School
 
-    # @@school_grade = []
-
+    attr_reader :roster
+    
     def initialize(school_name)
         @name = school_name
         @roster = {}
     end
 
-    def roster
-        @roster
-    end
-
     def add_student(student_name, grade)
-        if !@roster.key?(grade)
-            @roster[grade] = []
+        if !self.roster.key?(grade)
+            self.roster[grade] = []
         end
-        @roster[grade] << student_name
+        self.roster[grade] << student_name
     end
 
     def grade(grade_request)
-        @roster[grade_request]
+        self.roster[grade_request]
     end
 
     def sort
-        @roster = @roster.map do |grade, students|
-            binding.pry
-            {grade.sort => students.sort}
+        self.roster.each do |grade, students|
+            self.roster[grade] = students.sort
         end
     end
+
 end
