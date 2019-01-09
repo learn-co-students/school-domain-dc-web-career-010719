39ba9a63342a48690b4951de489dd2413dcde4b2
Ross Jones <rossmosis.jones@gmail.com>
2019-01-09 18:06:32 -0500

    small change

--- lib/school.rb
+++ lib/school.rb
@@ -3,14 +3,8 @@ require 'pry'
 
 class School
 
-<<<<<<< HEAD
     attr_reader :roster
     
-=======
-    # @@school_grade = []
-    attr_reader :roster
-
->>>>>>> 892e71713297f4502222004758201a75a5707282
     def initialize(school_name)
         @name = school_name
         @roster = {}
@@ -28,17 +22,8 @@ class School
     end
 
     def sort
-<<<<<<< HEAD
-        self.roster.each do |grade, students|
-            self.roster[grade] = students.sort
-        end
-    end
-
-end
-=======
         @roster.each do |grade, students|
           @roster[grade] = students.sort
         end
     end
 end
->>>>>>> 892e71713297f4502222004758201a75a5707282
