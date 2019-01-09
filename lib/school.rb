455d72858ec592f1c44d1c1fcf752f86ec10a18f
Ross Jones <rossmosis.jones@gmail.com>
2019-01-09 16:33:05 -0500

    added most methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+
+class School
+
+    # @@school_grade = []
+
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if !@roster.key?(grade)
+            @roster[grade] = []
+        end
+        @roster[grade] << student_name
+    end
+
+    def grade(grade_request)
+        @roster[grade_request]
+    end
+
+    def sort
+        @roster = @roster.map do |grade, students|
+            binding.pry
+            {grade.sort => students.sort}
+        end
+    end
+end
