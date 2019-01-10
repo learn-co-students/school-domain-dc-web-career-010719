279cd6889fb5560b5d0cf8fb4d8b6f906a60a286
Dusty211 <kylebenz@hotmail.com>
2019-01-10 09:00:03 -0500

    All tests pass.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+
+    @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    return_hash = {}
+    @roster.each {|grade, students| return_hash[grade] = students.sort}
+    return_hash
+  end
+
+end
